16a171e95a42334aa5c161e279ed988ead0fa444
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2023-03-30 18:19:21 -0300

    Configuração do loading

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { useFonts, Inter_400Regular, Inter_700Bold } from "@expo-google-fonts/inter";
+import { HomeScreen } from "./src/screens";
+import { Loading } from "./src/components/Loading";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  })
+
+  return (
+    <>
+      {fontsLoaded ? <HomeScreen /> : <Loading />}
+      
+    </>
+  );
+}
